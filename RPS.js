f7af33a7634ec1e8de8b71663533b80f97718ecc
caspar <caspareggert@gmail.com>
2018-03-25 15:21:51 +0100

    Changed initial function to give a word and number to help later functions

--- RPS.js
+++ RPS.js
@@ -1,27 +1,43 @@
-//Javascript file for Rock Paper Scissors
-function computerPlay (){
-  let random = Math.floor(Math.random() * 3);
-  if(random === 0){
-    return "rock"
-  } else if (random === 1){
-    return "paper"
-  } else {
-    return "scissors"
-  }
-}
+//Function that creates either rock, paper or scissors from a random number between 1& 3.
 
+let computerSelection;
 let playerSelection;
-let computerSelction;
 
-function winner (playerSelection, computerSelection){
-  if ('rock', 'paper'){
-    return "You lose Paper beats Rock" //This works but not the parts below.
-  } if ('paper', 'rock'){
-    return "You win! Paper beats Rock"
-  } else if ('paper', 'Scissors'){
-    return "You lose! Scissors beats paper"
+function computerPlay (){
+  let computerSelection = Math.floor(Math.random() * 3);
+  switch (computerSelection){
+    case 0:
+      return ["rock", 0];
+    case 1:
+      return ["paper", 1];
+    default:
+      return ["scissors", 2];
+  }
+}
+
+//Function that creates a single game of Rock Paper Scissors.
+
+
+
+function playRound(playerSelection, computerSelection) {
+  let selectedMessage = `You Selected: ${playerSelection[0]} and the computer selected: ${computerSelection[0]}`;
+  let winMessage = "You won this round!";
+  let loseMessage = "You lose this round, better luck next time!";
+
+  if (playerSelection[1] === 0 && computerSelection[1] === 2){
+   return [`${selectedMessage}: ${winMessage}`, 1];
+ } else if (playerSelection[1] === 1 && computerSelection[1] === 0) {
+   return [`${selectedMessage}: ${winMessage}`, 1];
+  } else if (playerSelection[1] === 2 && computerSelection[1] === 1) {
+   return [`${selectedMessage}: ${winMessage}`, 1];
+  } else if (playerSelection[1] === 0 && computerSelection[1] === 1) {
+   return [`${selectedMessage}: ${loseMessage}`, 0];
+  } else if (playerSelection[1] === 1 && computerSelection[1] === 2) {
+   return [`${selectedMessage}: ${loseMessage}`, 0];
+ } else if (playerSelection[1] === 2 && computerSelection[1] === 0) {
+   return [`${selectedMessage}: ${loseMessage}`, 0];
+  } else {
+   return [`${selectedMessage}: This round is a draw!`, 2];
   }
 
 }
-
-winner ('paper', 'scissors');
