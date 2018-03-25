0354e768f3e9bf9fdd28f7f70c733e8eb9707e67
caspar <caspareggert@gmail.com>
2018-03-25 16:12:58 +0100

    One round of rock paper scissors function completed

--- RPS.js
+++ RPS.js
@@ -1,43 +1,34 @@
 //Function that creates either rock, paper or scissors from a random number between 1& 3.
 
-let computerSelection;
-let playerSelection;
-
 function computerPlay (){
   let computerSelection = Math.floor(Math.random() * 3);
   switch (computerSelection){
     case 0:
-      return ["rock", 0];
+      return ["rock"];
     case 1:
-      return ["paper", 1];
+      return ["paper"];
     default:
-      return ["scissors", 2];
+      return ["scissors"];
   }
 }
 
 //Function that creates a single game of Rock Paper Scissors.
 
-
-
-function playRound(playerSelection, computerSelection) {
-  let selectedMessage = `You Selected: ${playerSelection[0]} and the computer selected: ${computerSelection[0]}`;
-  let winMessage = "You won this round!";
-  let loseMessage = "You lose this round, better luck next time!";
-
-  if (playerSelection[1] === 0 && computerSelection[1] === 2){
-   return [`${selectedMessage}: ${winMessage}`, 1];
- } else if (playerSelection[1] === 1 && computerSelection[1] === 0) {
-   return [`${selectedMessage}: ${winMessage}`, 1];
-  } else if (playerSelection[1] === 2 && computerSelection[1] === 1) {
-   return [`${selectedMessage}: ${winMessage}`, 1];
-  } else if (playerSelection[1] === 0 && computerSelection[1] === 1) {
-   return [`${selectedMessage}: ${loseMessage}`, 0];
-  } else if (playerSelection[1] === 1 && computerSelection[1] === 2) {
-   return [`${selectedMessage}: ${loseMessage}`, 0];
- } else if (playerSelection[1] === 2 && computerSelection[1] === 0) {
-   return [`${selectedMessage}: ${loseMessage}`, 0];
-  } else {
-   return [`${selectedMessage}: This round is a draw!`, 2];
+function playRound (playerSelection, computerSelection){
+  if (playerSelection === 'rock' && computerSelection === 'paper'){
+    return ["You lose! Paper beats rock."];
+  } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+    return ["You win! Paper beats rock."];
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+    return ["You lose! Scissors beats paper"];
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+    return ["You win! Scissors beats paper."];
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+    return ["You lose! Rock beats scissors"];
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+    return ["You win! Rock beats scissors."];
   }
-
 }
+
+const playerSelection = 'rock'
+const computerSelection = computerPlay()
